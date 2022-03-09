112a7ddaec911105a4b278002185d6f49fb3d5a4
Zachary Boelter <zboelter@gmail.com>
2022-03-09 13:31:52 -0600

    DOM nodes set to variables

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//variables for game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+
+//variables for DOM nodes
+
+const message = document.querySelector('#message')
+const player1ScoreBoard = document.querySelector('#player1ScoreBoard')
+const player2ScoreBoard = document.querySelector('#player2ScoreBoard')
+const player1Dice = document.querySelector('#player1Dice')
+const player2Dice = document.querySelector('#player2Dice')
+const rollBtn = document.querySelector('#rollBtn')
+const resetBtn = document.querySelector('#resetBtn')
