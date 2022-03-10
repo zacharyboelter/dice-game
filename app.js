d2a882bd69e92f9c69f8d72e99eb414b37937faa
Zachary Boelter <zboelter@gmail.com>
2022-03-10 10:00:07 -0600

    score display, score update, fix active class issues, highlight player turn with darker color

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ let player1Turn = true
 //variables for DOM nodes
 
 const message = document.querySelector('#message')
-const player1ScoreBoard = document.querySelector('#player1ScoreBoard')
-const player2ScoreBoard = document.querySelector('#player2ScoreBoard')
+const player1ScoreBoard = document.querySelector('#player1Scoreboard')
+const player2ScoreBoard = document.querySelector('#player2Scoreboard')
 const player1Dice = document.querySelector('#player1Dice')
 const player2Dice = document.querySelector('#player2Dice')
 const rollBtn = document.querySelector('#rollBtn')
@@ -20,15 +20,17 @@ rollBtn.addEventListener('click', function(){
     if (player1Turn) {
         player1Score += randomNumber
         message.textContent = `Player 1 rolled a ${randomNumber}`
-        player1Dice.textContent += `${randomNumber}`
+        player1Dice.textContent = `${randomNumber}`
         player1Dice.classList.remove('active')
         player2Dice.classList.add('active')
+        player1ScoreBoard.innerHTML = player1Score;
     } else {
         player2Score += randomNumber
         message.textContent = `Player 2 rolled a ${randomNumber}`
-        player2Dice.textContent += `${randomNumber}`
-        player2Dice.classList.remove('acitve')
+        player2Dice.textContent = `${randomNumber}`
+        player2Dice.classList.remove('active')
         player1Dice.classList.add('active')
+        player2ScoreBoard.textContent = player2Score
     }
     player1Turn = !player1Turn;
 })
